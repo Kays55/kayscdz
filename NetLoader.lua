3dc375546c26f782917a63f4b599d0b660f5bc4b
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-01-06 22:57:34 -0300

    Update NetLoader.lua

--- NetLoader.lua
+++ NetLoader.lua
@@ -1,87 +1,87 @@
 --LoadMain
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/IngameMacroEditor.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/IngameMacroEditor.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/AutoquestInfo.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/AutoquestInfo.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/ChatConsoleTabs.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/ChatConsoleTabs.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/DurabilityMacro.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/DurabilityMacro.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/LastAttacker.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/LastAttacker.lua', function(script)
     assert(loadstring(script))()
  end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/NPCInteraction.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/NPCInteraction.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/PlayerList.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/PlayerList.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/PMTabs.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/PMTabs.lua', function(script)
     assert(loadstring(script))()
   end);
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Main/Teleport.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Main/Teleport.lua', function(script)
     assert(loadstring(script))()
  end);
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/CaveBoster.lua', function(script) 
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/AtualCaveBoster.lua', function(script) 
     assert(loadstring(script))() 
 end);
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/LibsVocs.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/LibsVocs.lua', function(script)
     assert(loadstring(script))()
   end);
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/_vlib.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/_vlib.lua', function(script)
     assert(loadstring(script))()
   end);
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/PosVerification.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/PosVerification.lua', function(script)
     assert(loadstring(script))()
   end);
 
 --LoadAtk
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Atk.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Atk.lua', function(script)
     assert(loadstring(script))()
   end);
 
 
 --LoadDef
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Hp.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Hp.lua', function(script)
     assert(loadstring(script))()
   end);
 
 
 --LoadCave
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Cave.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Cave.lua', function(script)
     assert(loadstring(script))()
   end);
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/CaveCustons.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/CaveCustons.lua', function(script)
     assert(loadstring(script))()
   end);
 
 --LoadTarget
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Target.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Target.lua', function(script)
     assert(loadstring(script))()
   end);
 
 
 --Load Tools
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Tools.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Tools.lua', function(script)
     assert(loadstring(script))()
   end);
 
 
 --Load Discord
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/discord.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/discord.lua', function(script)
    assert(loadstring(script))()
   end);
 
 --Load Discord
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Atual/Bypassdoor.lua', function(script)
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Bypassdoor.lua', function(script)
    assert(loadstring(script))()
   end);
