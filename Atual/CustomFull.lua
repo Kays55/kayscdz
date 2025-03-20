17d373af432d487c7971addfc97f1273a5812b26
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-03-20 16:43:58 -0300

    Update CustomFull.lua

--- Atual/CustomFull.lua
+++ Atual/CustomFull.lua
@@ -6635,7 +6635,39 @@ onTextMessage(function(mode, text)
 end)
 
 
-loaded = true
+-- load all otui files, order doesn't matter
+local configName = modules.game_bot.contentsPanel.config:getCurrentOption().text
+
+local configFiles = g_resources.listDirectoryFiles("/bot/" .. configName .. "/vBot", true, false)
+for i, file in ipairs(configFiles) do
+  local ext = file:split(".")
+  if ext[#ext]:lower() == "ui" or ext[#ext]:lower() == "otui" then
+    g_ui.importStyle(file)
+  end
+end
+
+local function loadScript(name)
+  return dofile("/vBot/" .. name .. ".lua")
+end
+
+-- here you can set manually order of scripts
+-- libraries should be loaded first
+local luaFiles = {
+  "items",
+  "vlib",
+  "new_cavebot_lib",
+  "configs", -- do not change this and above
+  "cavebot",
+  "analyzer",
+  "depositer_config",
+}
+
+for i, file in ipairs(luaFiles) do
+  loadScript(file)
+end
+
+
 info(loaded)
 
-version = 1.3
+loaded = true
+version = 1.4
