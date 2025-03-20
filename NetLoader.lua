13b96109bc1c7dbef4fd588975fd905f52566d39
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-03-20 18:28:13 -0300

    Update NetLoader.lua

--- NetLoader.lua
+++ NetLoader.lua
@@ -1,9 +1,17 @@
 --LoadMain
+setDefaultTab("Main")
+setDefaultTab("Atk")
+setDefaultTab("Def")
+setDefaultTab("Cave")
+setDefaultTab("Target")
+setDefaultTab("Tools")
+setDefaultTab("Dsc")
+
 modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/CustomFull.lua', function(script)
     assert(loadstring(script))()
   end);
 
 
-modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Extras.lua', function(script)
-    assert(loadstring(script))()
-  end);
+--modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Extras.lua', function(script)
+--    assert(loadstring(script))()
+--  end);
