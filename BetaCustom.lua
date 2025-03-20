e813fb8cf773c1ecc1da2d265ca542fbd9d1175c
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-03-20 16:16:51 -0300

    Create BetaCustom.lua

---
+++ BetaCustom.lua
@@ -0,0 +1,9 @@
+--LoadMain
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/CustomFull.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/Kays55/kayscdz/refs/heads/main/Atual/Extras.lua', function(script)
+    assert(loadstring(script))()
+  end);
