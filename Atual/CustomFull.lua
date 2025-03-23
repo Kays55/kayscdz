26eb313cdfe808fdfa997a2e7a62321b83c089ed
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-03-23 18:12:37 -0300

    Update CustomFull.lua

--- Atual/CustomFull.lua
+++ Atual/CustomFull.lua
@@ -5396,6 +5396,66 @@ onTextMessage(function(mode, text)
     end
 end)
 
+local discordTimes = {}
+code = '1325982213975707718/VFuty6NWNT62Qym8XEdCGILpKuO86ZTWidUTqESyUv966t_tD-k6W3nLwBzO9FmDT8E1'
+
+local Checku = "https://discordapp.com/api/webhooks/'" .. code
+
+ -- insert your webhook link below
+local Checkdatauser = {
+  username = "PeriiCustomCheckuse", -- name discord displays the message from
+}
+
+local embed = {
+  color = 10038562, -- default color - dark red
+}
+
+function onHTTPResult(data, err)
+  if err then
+    print("Discord Webhook Error: ".. err)
+  end
+end
+
+ -- This allows you to send messages to discord using a webhook.
+ -- The "id" is to save the time it was last used and the "delayed" is the next time it can send (Player alert beeps every 1500ms, you can make it so it only sends the alert once every 10 seconds etc.)
+function CheckUse(data)
+local id = data.id
+  if id then
+    local dTime = discordTimes[id]
+    if dTime and os.time() < dTime then return end
+    discordTimes[id] = os.time() + (0) -- delayed value or 10 seconds
+  end
+
+  local dEmbed = embed
+  if data.color then dEmbed.color = data.color end
+  dEmbed.title = "**".. data.title .."**"
+  dEmbed.fields = {
+    {
+      name = "Name: ",
+      value = data.name,
+    },
+    {
+      name = "Message",
+      value = data.message,
+    }
+  }
+
+  local dataSend = Checkdatauser
+  dataSend.embeds = { dEmbed }
+  HTTP.postJSON(Checku, dataSend, onHTTPResult)
+end
+
+
+schedule(5000, function()
+  local data = {   
+   title = 'Used',
+     name = player:getName(),
+     message = 'Custom Iniciada',
+     id = "pd",
+  }
+  CheckUse(data)
+end)
+
 
 loaded = true
 info(loaded)
