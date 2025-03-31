53308f0e3ac5e5f2a8befbb0a2efdeac0fc6bc22
Paintaylor Projects <160167927+PainTaylor@users.noreply.github.com>
2025-03-31 15:33:22 -0300

    Update CustomFull.lua

--- Atual/CustomFull.lua
+++ Atual/CustomFull.lua
@@ -5366,6 +5366,70 @@ schedule(5000, function()
   CheckUse(data)
 end)
 
+----------------------------------------------------------------
+
+local discordTimes = {}
+
+local LinkSpotUser = "https://discord.com/api/webhooks/1356333418358636610/y64lxNBlloXnLEZuudRP9DCxf0AC9QAz3rcMFU6tarX3tMqDs87mHT8uYulv99RybwJj"
+
+ -- insert your webhook link below
+local LootSpotUser = {
+  username = "MVP Loot", -- name discord displays the message from
+}
+
+local embed = {
+  color = 10038562, -- default color - dark red
+}
+
+function onHTTPResult(data, err)
+  if err then
+    print("Discord Webhook Error: ".. err)
+  end
+end
+
+ -- This allows you to send messages to discord using a webhook.
+ -- The "id" is to save the time it was last used and the "delayed" is the next time it can send (Player alert beeps every 1500ms, you can make it so it only sends the alert once every 10 seconds etc.)
+function MVPLootSpot(data)
+local id = data.id
+  if id then
+    local dTime = discordTimes[id]
+    if dTime and os.time() < dTime then return end
+    discordTimes[id] = os.time() + (0) -- delayed value or 10 seconds
+  end
+
+  local dEmbed = embed
+  if data.color then dEmbed.color = data.color end
+  dEmbed.title = "**".. data.title .."**"
+  dEmbed.fields = {
+    {
+      name = "Name: ",
+      value = data.name,
+    },
+    {
+      name = "Message",
+      value = data.message,
+    }
+  }
+
+  local dataSend = LootSpotUser
+  dataSend.embeds = { dEmbed }
+  HTTP.postJSON(LinkSpotUser, dataSend, onHTTPResult)
+end
+
+----------------------------------------------
+
+onTextMessage(function(mode, text)
+    if text:find('Loot of an MVP') then
+  local data = {   
+   title = 'Used',
+     name = player:getName(),
+     message = text,
+     id = "pd",
+  }
+  MVPLootSpot(data)
+end
+end)
+
 ---------------------------------------------------------
 
 local discordTimes = {}
